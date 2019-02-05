8991d36b0ee4d756b25e7bfff982ca6b78bdb28e
charan41 <45058003+charan41@users.noreply.github.com>
2019-02-05 09:33:43 +0530

    Add files via upload

---
+++ src/main/java/com/mycompany/training/App.java
@@ -0,0 +1,13 @@
+package com.mycompany.training;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! version 3.0" );
+    }
+}
